3f933493d842713a053e6f5f8e134ec681a30223
sarma-nci-nih-essex <sarma.seetamraju@nih.gov>
2024-12-04 15:48:48 -0500

    FIX: code-cleanup

--- aws_tkt/aws_tkt_stack.py
+++ aws_tkt/aws_tkt_stack.py
@@ -1,33 +1,15 @@
-import os
 from aws_cdk import (
     Stack,
-    aws_lambda,
-    DockerImage,
-    DockerVolume,
-    BundlingOptions,
-    BundlingOutput,
-    SymlinkFollowMode,
-    RemovalPolicy,
 )
 from constructs import Construct
 
-import aws_tkt.constants as constants
 from .lambda_layer_builder import LambdaLayerBuilder
-from pipeline import AppDeployPipelineStack
 
 class AwsTktStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # LambdaLayerBuilder( self, "LambdaLayerBuilder" )
-
-        AppDeployPipelineStack( scope=self,
-            stack_id=self.stack_name,
-            git_repo_name=constants.git_repo_name,
-            git_repo_org_name=constants.git_repo_org_name,
-            pipeline_source_gitbranch="main",
-            codestar_connection_arn=None,
-        )
+        LambdaLayerBuilder( self, "LambdaLayerBuilder" )
 
 ### EoF
