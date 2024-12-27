2ec19ef9e58dbc803320abc9a4085631cec01561
sarma-nci-nih-essex <sarma.seetamraju@nih.gov>
2024-12-27 16:08:48 -0500

    FIX: rewrite to leverage LATEST custom-codebase/libraries for LambdaLayers (a.k.a. fix impact of deleting Lambda_Layer_Builder.py)

--- aws_tkt/application_stack.py
+++ aws_tkt/application_stack.py
@@ -1,19 +1,32 @@
 from aws_cdk import (
     Stack,
+    RemovalPolicy,
 )
-from constructs import Construct
 
-from .lambda_layer_builder import LambdaLayerBuilder
+from constructs import Construct
+from backend.common_aws_resources_stack import CommonAWSResourcesStack
 
 class AwsTktApplicationStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(
-            scope,
-            construct_id,
+    def __init__(self,
+        scope: Construct,
+        construct_id: str,
+        tier: str,
+        aws_env :str,
+        git_branch :str,
+        **kwargs
+    ) -> None:
+        super().__init__( scope, construct_id,
             stack_name=construct_id,
-            **kwargs)
+            **kwargs
+        )
 
-        LambdaLayerBuilder( self, "LambdaLayerBuilder" )
+        CommonAWSResourcesStack( scope=self, id_="CommonAWSRrcs",
+            tier=tier,
+            aws_env=aws_env,
+            git_branch=git_branch,
+            # lambda_configs = lambda_configs,
+            **kwargs,
+        )
 
 ### EoF
