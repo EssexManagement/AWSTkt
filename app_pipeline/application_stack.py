03e970da07da12422100a66b25538fe6c104676e
sarma-nci-nih-essex <sarma.seetamraju@nih.gov>
2024-12-30 18:50:22 -0500

    FIX: AwsTktApplicationStacks is NO longer a stack.  Just a plain-python-class.  Also, code is now sensitive to environ-var CPU_ARCH; Fix stack-naming.

--- app_pipeline/application_stack.py
+++ app_pipeline/application_stack.py
@@ -1,3 +1,4 @@
+import os
 from aws_cdk import (
     Stack,
     RemovalPolicy,
@@ -6,10 +7,14 @@ from aws_cdk import (
 from constructs import Construct
 
 import constants as constants
+import common.cdk.constants_cdk as constants_cdk
 import common.cdk.aws_names as aws_names
-from backend.common_aws_resources_stack import CommonAWSResourcesStack
+from cdk_utils.CloudFormation_util import get_cpu_arch_enum
+from backend.common_aws_resources_stack import CommonAWSResourcesStack, LAYER_MODULES
 
-class AwsTktApplicationStack(Stack):
+class AwsTktApplicationStacks():
+    """ Creates all the various Application's stacks.  This is in itself a PLAIN-Python-class!!!
+    """
 
     def __init__(self,
         scope: Construct,
@@ -19,19 +24,28 @@ class AwsTktApplicationStack(Stack):
         git_branch :str,
         **kwargs
     ) -> None:
-        super().__init__( scope, construct_id,
-            stack_name=construct_id,
-            **kwargs
-        )
+        HDR = " AwsTktApplicationStacks()'s constructor inside "+ __file__
+        # super().__init__( scope, construct_id )
 
         stk_prefix = aws_names.gen_awsresource_name_prefix( tier, constants.CDK_COMPONENT_NAME )
 
-        CommonAWSResourcesStack( scope=self, id_="CommonAWSRrcs",
-            tier=tier,
+        cpu_arch_str: str = os.environ.get("CPU_ARCH", None)
+        print( f"CPU_ARCH (Env-Var) = '{cpu_arch_str}' within "+ HDR )
+        cpu_arch = get_cpu_arch_enum( cpu_arch_str )
+        # cpu_arch_str: str = cpu_arch.name.lower()  ### === 'arm64|x86_64' string
+        print( f"CPU-ARCH (Enum) ='{cpu_arch}'" )
+
+        ### Stack # 1
+        CommonAWSResourcesStack(
+            scope = scope,
+            id = f"{construct_id}-CommonAWSRrcs-{cpu_arch_str}", ### <-------- note!! Using the param(construct_id) to create a NEW sub-construct-id!!!
+            tier = tier,
             aws_env=aws_env,
             git_branch=git_branch,
             # lambda_configs = lambda_configs,
             stk_prefix=stk_prefix,
+            cpu_arch_list = [cpu_arch],   ### Once cpu-arch at a time (as one CodeBuild-instance cannot handle 2 different cpu-arch)
+            # layer_modules_list = LAYER_MODULES,
             **kwargs,
         )
 
