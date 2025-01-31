bb56ca39f46664852bc1186af28e65d16d69014c
sarma-nci-nih-essex <sarma.seetamraju@nih.gov>
2025-01-30 21:23:34 -0500

    FIX: impact of renaming files in Lambda-layer folders

--- backend/lambda_layer/layers_config.py
+++ backend/lambda_layer/layers_config.py
@@ -1,10 +1,10 @@
 ### Attention: This is the authoritative list of all layers to be DEPLOYED !!!
 
-from backend.lambda_layer.psycopg.lambda_layer_psycopg import props as lambda_layer_psycopg
-from backend.lambda_layer.psycopg_pandas.lambda_layer_psycopg_pandas import props as lambda_layer_psycopg_pandas
-from backend.lambda_layer.psycopg3.lambda_layer_psycopg3 import props as lambda_layer_psycopg3
-from backend.lambda_layer.psycopg3_pandas.lambda_layer_psycopg3_pandas import props as lambda_layer_psycopg3_pandas
-from backend.lambda_layer.numpy_etc.lambda_layer_numpy_etc import props as lambda_layer_numpy_etc
+from backend.lambda_layer.psycopg.LayerConfig import props as lambda_layer_psycopg
+from backend.lambda_layer.psycopg_pandas.LayerConfig import props as lambda_layer_psycopg_pandas
+from backend.lambda_layer.psycopg3.LayerConfig import props as lambda_layer_psycopg3
+from backend.lambda_layer.psycopg3_pandas.LayerConfig import props as lambda_layer_psycopg3_pandas
+from backend.lambda_layer.numpy_etc.LayerConfig import props as lambda_layer_numpy_etc
 
 LAYER_MODULES = [
     lambda_layer_psycopg,
