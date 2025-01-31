ccf69bb6231f97dc3f3272e520a66f5eff91d7f7
sarma-nci-nih-essex <sarma.seetamraju@nih.gov>
2025-01-30 21:20:31 -0500

    UPDATE: copied from another fork

--- cdk_lambda_layers_app.py
+++ cdk_lambda_layers_app.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+### This is an alternate to the "standard CDK's"  `app.py`
+###
+### To be used as:
+###     npx cdk synth --app "python3 cdk_lambda_layers_app.py" -o cdk.out --quiet   \
+###                     -c tier=${TIER} -c git_repo=${GITHUB_REPOSITORY}    \
+###                     --profile ${AWSPROFILE} --region ${AWSREGION}
+
 import os
 import pathlib
 import time
@@ -11,19 +17,28 @@ from constructs import Construct
 import constants as constants
 import common.cdk.constants_cdk as constants_cdk
 import common.cdk.aws_names as aws_names
-from cdk_utils.CloudFormation_util import get_cpu_arch_enum, get_cpu_arch_as_str, add_tags
+from cdk_utils.CloudFormation_util import get_cpu_arch_enum,get_cpu_arch_as_str,  add_tags
 from backend.lambda_layer.lambda_layers_builder_stacks import LambdaLayersBuilderStacks
-from backend.AllStacks import AppStack
 
 ### ==============================================================================================
 ### ..............................................................................................
 ### ==============================================================================================
 
+app = cdk.App()
+
+scope :Construct = app
+
 HDR = " inside "+ __file__
 
-tier=constants.DEV_TIER
-aws_env = constants.DEV_TIER  ### <---- Hardcoded !!!
-git_branch = constants.get_git_branch(tier)
+tier :str = app.node.try_get_context("tier")
+git_branch :str = constants.get_git_branch( tier=tier )
+aws_env :str = constants.get_aws_env( tier=tier )
+if not tier or tier.lower().strip() == "":
+    print( f"!! ERROR !! tier is EMPTY == '{tier}'.  Pass in proper value via CDK's CLI-argument '--context tier=\"dev\"' !!!!!!!!" )
+    sys.exit(31)
+print( f"tier = '{tier}' within "+ __file__ )
+print( f"git_branch = '{git_branch}' within "+ __file__ )
+print( f"aws_env = '{aws_env}' within "+ __file__ )
 
 stk_prefix = aws_names.gen_awsresource_name_prefix( tier, constants.CDK_COMPONENT_NAME )
 
@@ -37,15 +52,6 @@ env = cdk.Environment(
 ### ==============================================================================================
 ### ..............................................................................................
 ### ==============================================================================================
-
-app = cdk.App()
-
-scope :Construct = app
-
-""" Creates all the various Application's stacks.  This is in itself a PLAIN-Python-class!!!
-"""
-
-
 
 # cpu_arch_str: str = os.environ.get("CPU_ARCH", None)
 cpu_arch_str: str = app.node.try_get_context( 'CPU_ARCH' )
@@ -65,6 +71,8 @@ if cpu_arch_str != get_cpu_arch_as_str(cpu_arch):
     print()
     sys.exit( 4 )
 
+### ...............................
+
 aws_profile = app.node.try_get_context( 'AWSPROFILE' )
 ### detect if running on macos/windows LAPTOP --versus-- running inside AWS-CodeBuild
 if platform.system() == 'Darwin' or platform.system() == "Windows":
@@ -83,7 +91,7 @@ from backend.lambda_layer.bin.get_lambda_layer_hashes import GetHashesForLambdaL
 ### Dynamically update the code in the file `backend/lambda_layer/lambda_layer_hashes.py` (with the latest sha256-hashes downloaded from AWS)
 GetHashesForLambdaLayers(
     appl_name = constants.CDK_APP_NAME,
-    aws_profile = aws_profile,
+    aws_profile = aws_profile, ### Note: when running inside CodeBuild, `awsprofile` -MUST- be None.
     tier = tier,
     file_to_save_hashes_into = pathlib.Path( LAMBDA_LAYER_HASHES_LOCALFILEPATH ),
     # purpose = THIS_SCRIPT_DATA,
@@ -95,31 +103,17 @@ with open( LAMBDA_LAYER_HASHES_LOCALFILEPATH, 'r' ) as f:
 
 ### ..............................................................................................
 
+### Create all the various Application's stacks.
+
 ### Stack # 1
 common_stk = LambdaLayersBuilderStacks(
     scope = scope,
-    simple_id = f"LambdaLayers-{cpu_arch_str}",
+    simple_id = f"CommonRsrc-{cpu_arch_str}",
     stk_prefix = stk_prefix,
     tier = tier,
     aws_env=aws_env,
     git_branch=git_branch,
     cpu_arch_list = [cpu_arch],   ### Once cpu-arch at a time (as one CodeBuild-instance cannot handle 2 different cpu-arch)
-
-    env = env,  ### kwargs !!!
-)
-
-### ..............................................................................................
-
-### Stack # 2
-AppStack(
-    scope = scope,
-    simple_id = f"AppStk-{cpu_arch_str}",
-    stk_prefix = stk_prefix,
-    tier = tier,
-    aws_env=aws_env,
-    git_branch=git_branch,
-    cpu_arch_str = cpu_arch_str,
-    common_stk = common_stk,
 
     env = env,  ### kwargs !!!
 )
