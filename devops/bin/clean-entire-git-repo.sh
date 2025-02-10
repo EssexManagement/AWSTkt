0c3dda996ef20e3846094a0bc918c4fb0f500792
sarma-nci-nih-essex <sarma.seetamraju@nih.gov>
2025-02-10 01:18:01 -0500

    CHORE: bash-script to cleanup entire project-git-repo

--- devops/bin/clean-entire-git-repo.sh
+++ devops/bin/clean-entire-git-repo.sh
@@ -18,11 +18,16 @@ SrcPaths=(
 
 ### ===============================================================================
 
-### Topmost folder of project.
+### Run following at the Topmost folder of project.
+
+echo "rm -rf ./node_modules/"
 \rm -rf "./node_modules/"
+
+echo "rm -rf ./cdk.out/"
 \rm -rf "./cdk.out/"
 
 ### For each subfolder listed above ..
+echo "wiping out __pycache__ sub-folders everywhere ..."
 for dd in ${SrcPaths[@]}; do
     find ${dd} -name '__pycache__' -exec rm -rf {} \;
 done
